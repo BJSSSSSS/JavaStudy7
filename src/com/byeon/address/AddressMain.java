b413013e5447dbbf639a088bda39dd51404d1a05
BJSSSSSS <john0222223@gmail.com>
2022-01-13 16:33:45 +0900

    addDTO(2022.01.13)

---
+++ src/com/byeon/address/AddressMain.java
@@ -0,0 +1,10 @@
+package com.byeon.address;
+
+public class AddressMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/byeon/address/HumanDTO.java
@@ -0,0 +1,45 @@
+package com.byeon.address;
+
+public class HumanDTO {
+
+	private String name;
+	private String nick;
+	private String email;
+	private String tel;
+	private String birthday;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setId(String nick) {
+		this.nick = nick;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	
+	
+	
+}
