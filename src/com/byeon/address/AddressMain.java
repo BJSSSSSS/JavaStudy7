70b6181000905d35e84efe1148bd6fce4f814ad2
BJSSSSSS <john0222223@gmail.com>
2022-01-14 17:51:40 +0900

    lastlast(2022.01.14)

--- src/com/byeon/address/AddressMain.java
+++ src/com/byeon/address/AddressMain.java
@@ -1,7 +1,7 @@
 package com.byeon.address;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.HashMap;
 
 public class AddressMain {
 
@@ -11,14 +11,14 @@ public class AddressMain {
 		
 		humanMenu.menu();
 	
-		ArrayList<Integer> ar = new ArrayList<Integer>();
-		ArrayList<Integer> ar2 = new ArrayList<Integer>();
-
-		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
-		
-		map.put("a", ar);
-		map.put("b", ar2);
-		
+//		ArrayList<Integer> ar = new ArrayList<Integer>();
+//		ArrayList<Integer> ar2 = new ArrayList<Integer>();
+//
+//		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+//		
+//		map.put("a", ar);
+//		map.put("b", ar2);
+//		
 		
 	}
 
