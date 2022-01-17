e4d666eb930c2a6acfd0436670e9f1698f87d908
BJSSSSSS <john0222223@gmail.com>
2022-01-17 17:50:48 +0900

    last(2022.01.17)

---
+++ src/com/byeon/address/TestMain.java
@@ -0,0 +1,18 @@
+
+
+package com.byeon.address;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		//System.out.println("Test", "error" );
+		HumanView view = new HumanView();
+		HumanDTO humanDTO = null;
+		System.out.println("실행 중");
+		view.humanView(humanDTO);
+
+	}
+
+}
+
+
